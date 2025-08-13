f61666ad0281fe3e6db315a513ba9ffcce0a07b5
nishikant-zensar <nishikant.sinha@zensar.com>
2025-08-13 20:43:31 +0530

    Create provider.tf

---
+++ modules/tf-rg-test/provider.tf
@@ -0,0 +1,7 @@
+provider "azurerm" { 
+  features {}
+  alias           = "zencislab"
+  subscription_id = "eca8a48a-2dc4-45da-908c-94bf6100016c"
+  tenant_id       = "684d2402-0ea6-442d-9ad7-4ef26b925ec5"
+  client_id       = "74925104-cd8b-47e5-b29a-83a75a2f4ca6"
+}
