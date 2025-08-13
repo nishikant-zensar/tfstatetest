c2d7e40ac473ac2781010e1b52841c44c7217267
nishikant-zensar <nishikant.sinha@zensar.com>
2025-08-13 20:16:36 +0530

    Create rg.tf

---
+++ modules/tf-rg-test/rg.tf
@@ -0,0 +1,33 @@
+terraform {
+  backend "azurerm" {
+    resource_group_name  = "ims-prd-lz-ne-rg-terraformstate"
+    storage_account_name = "imslandingznstr"
+    container_name       = "tfstate"
+    key                  = "hubspoke.terraform.tfstate" # Path to the state file in the container
+    use_oidc_auth        = true
+    use_azuread_auth     = true
+  }
+  required_providers {
+    azurerm = {
+      source  = "hashicorp/azurerm"
+      version = "~> 4.0"
+    }
+  }
+  required_version = ">= 1.0"  
+}
+
+provider "azurerm" {
+  features {}
+}
+# Create Network RG in Connectivity, Managemnet and AVD MG
+# 1. Resource Group in ims-prd-connectivity (Connectivity subscription) 
+resource "azurerm_resource_group" "conn" {
+  provider = azurerm.ims-prd-connectivity
+  name     = "ims-prd-conn-ne-rg-network"
+  location = var.location
+  tags = {
+    Name        = "ims-prd-conn-ne-rg-network"
+    Environment = "prd"
+    DateCreated = "2025-08-01"
+  }
+  }
