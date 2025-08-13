b63d0838e5ba30d530a75f78ef2822a840aaaf21
nishikant-zensar <nishikant.sinha@zensar.com>
2025-08-13 20:41:47 +0530

    Update rg.tf

--- modules/tf-rg-test/rg.tf
+++ modules/tf-rg-test/rg.tf
@@ -3,7 +3,7 @@ terraform {
     resource_group_name  = "tftest-rg"
     storage_account_name = "tfteststorageacc1"
     container_name       = "tfstate"
-    key                  = "hubspoke.terraform.tfstate" # Path to the state file in the container
+    key                  = "tfimport.terraform.tfstate" # Path to the state file in the container
     use_oidc_auth        = true
     use_azuread_auth     = true
   }
@@ -20,8 +20,20 @@ provider "azurerm" {
   features {}
 }
 # Create Network RG in Connectivity, Managemnet and AVD MG
-# 1. Resource Group in ims-prd-connectivity (Connectivity subscription) 
+# 1. Resource Group in zencislab 
 resource "azurerm_resource_group" "conn" {
+  provider = azurerm.ims-prd-connectivity
+  name     = "ims-prd-conn-ne-rg-network"
+  location = var.location
+  tags = {
+    Name        = "ims-prd-conn-ne-rg-network"
+    Environment = "prd"
+    DateCreated = "2025-08-01"
+  }
+  }
+
+# 2. Resource Group in zencislab 
+resource "azurerm_resource_group" "conn1" {
   provider = azurerm.ims-prd-connectivity
   name     = "ims-prd-conn-ne-rg-network"
   location = var.location
