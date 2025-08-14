944fb8ac452d42a684d522813685b5a17d420f4f
nishikant-zensar <nishikant.sinha@zensar.com>
2025-08-14 18:12:27 +0530

    Update variables.tf

--- modules/tf-rg-test/variables.tf
+++ modules/tf-rg-test/variables.tf
@@ -3,3 +3,9 @@ variable "location" {
   type        = string
   default     = "northeurope"
 }
+variable "org" { default = "ims" }
+variable "env" { default = "prd" }
+variable "sub" { default = "conn" }
+variable "region" { default = "ne" }
+variable "type" { default = "rg" }
+variable "suffix" { default = "network" }
